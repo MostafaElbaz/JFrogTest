36097fadbb915f0a9a84eedfde7b1bd98fea20ed
Mostafa Elbaz <Mostafa.elbaz@vodafone.com>
2025-10-16 21:17:05 +0300

    no message

---
+++ jFrogTest/jFrogTest.h
@@ -0,0 +1,18 @@
+//
+//  jFrogTest.h
+//  jFrogTest
+//
+//  Created by Mostafa Elbaz on 16/10/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for jFrogTest.
+FOUNDATION_EXPORT double jFrogTestVersionNumber;
+
+//! Project version string for jFrogTest.
+FOUNDATION_EXPORT const unsigned char jFrogTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jFrogTest/PublicHeader.h>
+
+
---
+++ jFrogTest/setup.swift
@@ -0,0 +1,13 @@
+//
+//  Untitled.swift
+//  jFrogTest
+//
+//  Created by Mostafa Elbaz on 16/10/2025.
+//
+public struct setup {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        print("Hello, World!")
+    }
+}
